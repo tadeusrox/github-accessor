724b6ef6d46b5c4cff0e58303ecedfdfd2806caa
Vinicius Tadeu <tadeusrox@gmail.com>
2016-05-26 12:39:13 -0300

    Create repository model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526153613) do
+ActiveRecord::Schema.define(version: 20160526153845) do
+
+  create_table "repositories", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "full_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "repositories", ["user_id"], name: "index_repositories_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "github_login"
