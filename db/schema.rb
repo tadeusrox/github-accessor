bc47ae7d5333f9c03d39da421006fa1156f6d856
Vinicius Tadeu <tadeusrox@gmail.com>
2016-05-26 12:44:42 -0300

    Remove cache key from user model

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20160526153845) do
 
   create_table "users", force: :cascade do |t|
     t.string   "github_login"
-    t.string   "cache_key"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
