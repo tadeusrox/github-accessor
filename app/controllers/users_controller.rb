cc4cd6b579490417af0aa352c48e40d77cbdda60
Vinicius Tadeu <tadeusrox@gmail.com>
2016-05-26 12:16:21 -0300

    List all repositories on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    if current_user.login == params[:name]
+      @user = current_user
+    else
+      redirect_to root_path
+    end
   end
 end
