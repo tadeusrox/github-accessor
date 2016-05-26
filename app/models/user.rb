f6ceb280a5e92fec3b1e506d6bc1c958be3fa1b6
Vinicius Tadeu <tadeusrox@gmail.com>
2016-05-26 13:02:42 -0300

    Get repositories from database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   attr_accessor :github_user
+
+  has_many :repositories
+
+  def list_repositories
+    github_repositories.each do |repo|
+      repositories.find_or_create_by(full_name: repo.full_name)
+    end
+    repositories
+  end
+
+  def github_repositories
+    Rails.cache.fetch("#{cache_key}/repositories", expires_in: 1.year) do
+      github_user.repositories
+    end
+  end
 end
