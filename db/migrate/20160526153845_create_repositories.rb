210f0135b7a790f497e630068562e66b6f899056
Vinicius Tadeu <tadeusrox@gmail.com>
2016-05-26 12:56:56 -0300

    Add index to repositories

--- db/migrate/20160526153845_create_repositories.rb
+++ db/migrate/20160526153845_create_repositories.rb
@@ -2,7 +2,7 @@ class CreateRepositories < ActiveRecord::Migration
   def change
     create_table :repositories do |t|
       t.references :user, index: true, foreign_key: true
-      t.string :full_name
+      t.string :full_name, index: true
 
       t.timestamps null: false
     end
