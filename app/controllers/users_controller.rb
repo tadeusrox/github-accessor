0f57af73a2bd8cd5a7b218b605c5a24887186990
Vinicius Tadeu <tadeusrox@gmail.com>
2016-05-26 13:02:42 -0300

    Get repositories from database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
     if current_user.login == params[:name]
-      @user = current_user
+      @user = User.find_or_create_by(github_login: current_user.login)
+      @user.github_user = current_user
     else
       redirect_to root_path
     end
